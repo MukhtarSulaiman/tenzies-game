5b6716c7991ae5d51d61b06a61ed3e2de681ba63
Mukhtar SULAIMAN <mukhtar.sulaiman.pro@gmail.com>
2022-10-05 22:54:13 +0200

    A timer has been added

--- src/App.js
+++ src/App.js
@@ -9,16 +9,33 @@ function App() {
 	const [dice, setDice] = React.useState(allNewDice());
 	const [tenzies, setTenzies] = React.useState(false);
 	const [rollsNumber, setRollsNumber] = React.useState(0);
-	
+	const [time, setTime] = React.useState(0);
+	const [timerOn, setTimerOn] = React.useState(false);
+
 	React.useEffect(() => {
 		const firstValue = dice[0].value;
 		const areAllHeld = dice.every((die) => die.isHeld);
 		const areAllSameValue = dice.every((die) => die.value === firstValue);
 		if (areAllHeld && areAllSameValue) {
 			setTenzies(true);
+			setTimerOn(false);
 			console.log('You won the game 🥳');
 		}
 	}, [dice]);
+
+	React.useEffect(() => {
+		let interval = null;
+
+		if (timerOn) {
+			interval = setInterval(() => {
+				setTime((prevTime) => prevTime + 10);
+			}, 10);
+		} else if (!timerOn) {
+			clearInterval(interval);
+		}
+
+		return () => clearInterval(interval);
+	}, [timerOn]);
 
 	// Helper function
 	function generateNewDie() {
@@ -40,17 +57,21 @@ function App() {
 	function handleClick(isGameEnded) {
 		if (isGameEnded) {
 			setDice(allNewDice());
-			setTenzies(false)
-			setRollsNumber(0)
+			setTenzies(false);
+			setRollsNumber(0);
+			setTime(0);
+			
+		} else {
+			setDice((oldDice) =>
+				oldDice.map((die) => {
+					return die.isHeld ? die : generateNewDie();
+				})
+			);
+			setRollsNumber((prevRollsNumber) => {
+				return (prevRollsNumber = prevRollsNumber + 1);
+			});
+			setTimerOn(true);
 		}
-		setDice((oldDice) =>
-			oldDice.map((die) => {
-				return die.isHeld ? die : generateNewDie();
-			})
-		);
-		setRollsNumber(prevRollsNumber => {
-			return prevRollsNumber = prevRollsNumber + 1
-		})
 	}
 
 	function handleSelectedNumber(id) {
@@ -72,6 +93,7 @@ function App() {
 				handleSelectedNumber={handleSelectedNumber}
 				isGameEnded={tenzies}
 				rollsNumber={rollsNumber}
+				timer={time}
 			/>
 		</main>
 	);
--- src/components/Die.js
+++ src/components/Die.js
@@ -10,20 +10,30 @@ const Die = (props) => {
 				{die.value}
 			</div>
 		);
-     })
+	})
 
      return (
 		<div>
 			<h1>Tenzies 🎲</h1>
 			<p>
-				{ props.isGameEnded ? 'You won the game! 🥳' : 'Roll until all dice are the same. Click each die to freeze it at its current value between rolls'}.
+				{props.isGameEnded
+					? 'You won the game! 🥳'
+					: 'Roll until all dice are the same. Click each die to freeze it at its current value between rolls'}
+				.
 			</p>
-			<div className="rolls-time-container">
-				<p>Rolls/{props.rollsNumber}</p>
-				<p>Time: </p>
+			<div className='rolls-time-container'>
+				<p>Rolls: {props.rollsNumber}</p>
+				<p>
+				Time: <span>{("0" + Math.floor((props.timer / 60000) % 60)).slice(-2)}:</span>
+					<span>{("0" + Math.floor((props.timer / 1000) % 60)).slice(-2)}:</span>
+					<span>{("0" + ((props.timer / 10) % 100)).slice(-2)}</span>
+				</p>
 			</div>
 			<div className='boxes-container'>{numbers}</div>
-			<button onClick={() => props.handleClick(props.isGameEnded)}> {props.isGameEnded ? 'New Game' : 'Roll'}</button>
+			<button onClick={() => props.handleClick(props.isGameEnded)}>
+				{' '}
+				{props.isGameEnded ? 'New Game' : 'Roll'}
+			</button>
 		</div>
 	);
 }
